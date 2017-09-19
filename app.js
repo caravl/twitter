0c28bbb14c9343083f84354d5e45da8185075f13
Cara Lang <caravlang@gmail.com>
2017-09-19 09:57:45 -0400

    add beginning setup

--- app.js
+++ app.js
@@ -4,13 +4,17 @@ const app = express(); // creates an instance of an express application
 
 
 app.get('/', function(request, response) {
-  response.send('Welcome')
+  response.send('Welcome');
 })
 
+app.use('/', function(request, response, next) {
+  console.log('partial match');
+  next();
+})
 
-
-
-
+app.use('/special', function(request, response, next) {
+  console.log("You've reached the special area")
+})
 
 
 
